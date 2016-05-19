4344fb7ee14de5adada95b476eb186bb806bc39c
Steve <Steve5805@gmail.com>
2016-05-18 22:48:55 -0400

    fix btoa import

--- routes/tokenize.js
+++ routes/tokenize.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var request = require('btoa');
+var btoa = require('btoa');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
