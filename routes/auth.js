1620a4cc629aa3b865109f3db897243d591e5661
john.sichina <john.sichina@F1710-JXGNJ12-L.ad.local>
2016-05-19 09:19:13 -0400

    remove "qs" package, use native "querystring" instead

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var qs = require('qs');
+var querystring = require('querystring');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	var tokenData = qs.parse(req.body.authTokenData);
+	var tokenData = querystring.parse(req.body.authTokenData);
 	var token = tokenData.token_type + ' ' + tokenData.access_token;
 	var maxAgeSecs = 0+tokenData.expires_in;
 	res.cookie('authToken', token, {maxAge: maxAgeSecs});
