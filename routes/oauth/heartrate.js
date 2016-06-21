82a525c282556c18001fa1aed466fb63a6f86ac5
Steve <steve5805@gmail.com>
2016-06-21 01:31:20 +0000

    add functional daterange feature: specify start and end date to add all intervening dates in one request. DB inserts tested and working!

--- routes/oauth/heartrate.js
+++ routes/oauth/heartrate.js
@@ -1,37 +1,66 @@
 'use strict';
 const router = require('express').Router();
-const request = require('request');
+const Promise = require('bluebird');
 const DB = require('../../models/DB');
 
+const httpGet = Promise.promisify(require('request').get);
+
 router.get('/', function(req, res, next) {
-	res.render('heartrate');
+	const today = getDateOnly(new Date());
+	res.render('heartrate', {dateStart:today, dateEnd:today});
 });
 
 router.post('/', function(req, res, next) {
-	const dateStart = req.body.dateStart;
-	const options = {
-		url: `https://api.fitbit.com/1/user/-/activities/heart/date/${dateStart}/1d/1sec.json`,
-		headers: {'Authorization': req.cookies.authToken}
-	};
-	request.get(options, function(err, response, body) {
-		if (err) {
-			next(err);
-		} else if (response.statusCode !== 200) {
-			next(body);
-		} else {
-			const data = JSON.parse(body);
-			processHeartData(data['activities-heart-intraday']['dataset']);
-			res.render('heartrate', {dateStart, heartrateData: JSON.stringify(data, null, 2)});	
-		}
-	});
+	
+	const inserts = [];
+	let insertCount = 0;
+	const curDate = new Date(req.body.dateStart);
+	const maxDate = new Date(req.body.dateEnd);
+	
+	while (curDate <= maxDate) {
+		let thisDate = getDateOnly(curDate);
+		
+		let options = {
+			url: `https://api.fitbit.com/1/user/-/activities/heart/date/${thisDate}/1d/1sec.json`,
+			headers: {'Authorization': req.cookies.authToken}
+		};
+		let getReq = httpGet(options).then(function(response) {
+			if (response.statusCode !== 200) {
+				next(response.body);
+			} else {
+				const data = JSON.parse(response.body);
+				const dataset = data['activities-heart-intraday']['dataset'];
+				return processHeartData(dataset, thisDate).tap(x => insertCount += x);
+			}
+		});
+		inserts.push(getReq);
+		
+		curDate.setDate(curDate.getDate() + 1);
+	}
+	
+	Promise.all(inserts).then(function(result) {
+		console.log(`Rendering after ${insertCount} records.`);
+		res.locals.dateStart = req.body.dateStart;
+		res.locals.dateEnd = req.body.dateEnd; 
+		res.locals.heartrateData = `Inserted: ${insertCount} records from ${req.body.dateStart} to ${req.body.dateEnd}.`;
+		res.render('heartrate');		
+	}).catch(next);
 });
 
-function processHeartData(heartData) {
+function processHeartData(heartData, date) {
+	console.log(`Inserting records for ${date}...`);
 	const inserts = [];
 	for (let heartrate of heartData) {
-		inserts.push([heartrate.time, heartrate.value]);
+		let timestamp = date + ' ' + heartrate.time;
+		inserts.push([timestamp, heartrate.value]);
 	}
-	DB.batchInsert('heartrate', ['timestamp', 'bpm'], inserts);
+	return DB.batchInsert('heartrate', ['timestamp', 'bpm'], inserts);
 }
+
+function getDateOnly(date) {
+	return date.toISOString().substr(0, 10)
+}
+
+
 
 module.exports = router;
